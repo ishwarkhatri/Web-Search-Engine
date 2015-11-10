e5e7e20312f22b9cbb42b183332bf977661d525c
ishwar27dec <ishwar.27dec@gmail.com>
2015-11-09 21:37:58 -0500

    Updated code as per instructions

---
+++ Tokenizer Project/src/edu/binghamton/my/common/Constants.java
@@ -0,0 +1,22 @@
+package edu.binghamton.my.common;
+
+public class Constants {
+
+	public static final String DICTIONARY_FILE_NAME = "Dictionary.csv";
+	public static final String DOCUMENTS_FILE_NAME = "Documents.csv";
+	public static final String POSTINGS_FILE_NAME = "Postings.csv";
+	public static final String SUMMARY_FILE_NAME = "Total.csv";
+	public static final String PROJECT_START_MESSAGE = "*** Tokenizer Project Part II ***";
+	public static final String FILE_PATH_ABSENT_ERROR_MESSAGE = "Document path not present!!";
+	public static final String FILE_INFO_FETCHING_MESSAGE = "\nFetching files info at: ";
+	public static final String FILES_TO_PROCESS_MESSAGE = " file(s) to process";
+	public static final String EXECUTION_START_MESSAGE = "Starting execution...";
+	public static final String TOKENIZING_INFO_MESSAGE = "Tokenizing file and creating dictionary";
+	public static final String TASK_COMPLETED_MESSAGE = "Task completed. Exporting data...";
+	public static final String EXPORTING_DOC_FILE_MESSAGE = "Exporting Documents file: " + DOCUMENTS_FILE_NAME;
+	public static final String EXPORTING_DICTIONARY_AND_POSTINGS_FILE_MESSAGE = "Exporting Dictionary file : " + DICTIONARY_FILE_NAME + " and Postings files : " + POSTINGS_FILE_NAME;
+	public static final String EXPORTING_SUMMARY_FILE_MESSAGE = "Exporting Summary file: " + SUMMARY_FILE_NAME;
+	public static final String FILES_EXPORT_COMPLETED_MESSAGE = "Files exported successfully\n";
+	public static final String PROGRAM_END_MESSGAE = "*** Tokenization Completed ***";
+	
+}
